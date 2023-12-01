b04739fc736e06eed75ed9711e2ca9afe42d817f
nabdallahsaab <97283136+nabdallahsaab@users.noreply.github.com>
2023-12-01 10:08:13 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemple de page avec PHP</title>
+</head>
+<body>
+
+<h1>Exemple de page avec PHP</h1>
+
+<?php
+// Script PHP pour afficher "Bonjour, monde !"
+echo "Bonjour, monde !";
+?>
+
+</body>
+</html>
